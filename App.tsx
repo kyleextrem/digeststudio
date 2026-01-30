e67b2f561c656727bfe24d231bc0b7d4c9a4e464
kyleextrem <kyle.extrem1@gmail.com>
2026-01-30 17:34:49 +1100

    Add Vercel Analytics

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/react";
 import Home from './pages/Home';
 import About from './pages/About';
 import FAQ from './pages/FAQ';
@@ -59,6 +60,7 @@ const App: React.FC = () => {
   return (
     <HashRouter>
       <ScrollToTop />
+      <Analytics />
       <Layout />
     </HashRouter>
   );
